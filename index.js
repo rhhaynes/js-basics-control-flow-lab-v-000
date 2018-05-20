750b8ebd4e380097e288d4a74d4c45d5ad709d04
Randall Harris Haynes <harrishaynes@gmail.com>
2018-05-20 19:32:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your code in this file!
+function scuberGreetingForFeet(distance){
+  let greeting;
+  if (distance <= 199) {
+    greeting = 'This one is on me!';
+  } else if (distance > 2000 && distance <= 2500) {
+    greeting = 'I will gladly take your thirty bucks.';
+  } else if (distance > 2500) {
+    greeting = 'No can do.';
+  }
+  return greeting;
+}
+
+function ternaryCheckCity(destination){
+  return (destination === 'NYC' ? 'Ok, sounds good.' : 'No go.');
+}
+
+function switchOnCharmFromTip(tip){
+  let charm;
+  switch(tip){
+    case 'generous':
+      charm = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      charm = 'Thank you.';
+      break;
+    default:
+      charm = 'Bye.';
+  }
+  return charm;
+}
