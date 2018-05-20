1cdbcae80cea03306c2ed22da079a555fec6b300
Randall Harris Haynes <harrishaynes@gmail.com>
2018-05-20 19:17:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function scuberGreetingForFeet(distance){
   let greeting;
   
-  if distance <= 199 {
+  if (distance <= 199) {
     greeting = 'This one is on me!'
-  } else if distance > 2000 && distance <= 2500 {
+  } else if (distance > 2000 && distance <= 2500) {
     greeting = 'I will glad take your thirty bucks.'
-  } else if distance > 2500 {
+  } else if (distance > 2500) {
     greeting = 'No can do.'
   }
   
